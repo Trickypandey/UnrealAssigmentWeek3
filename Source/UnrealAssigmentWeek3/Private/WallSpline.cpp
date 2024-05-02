4df2bf4e8e6f31c983fc80768f6e35bf496a7528
sourabh <sourabhpandey170@gmail.com>
2024-05-02 15:43:09 +0530

    added tha basic spline

--- Source/UnrealAssigmentWeek3/Private/WallSpline.cpp
+++ Source/UnrealAssigmentWeek3/Private/WallSpline.cpp
@@ -2,6 +2,7 @@
 
 
 #include "WallSpline.h"
+#include <Components/SplineMeshComponent.h>
 
 // Sets default values
 AWallSpline::AWallSpline()
@@ -9,6 +10,41 @@ AWallSpline::AWallSpline()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	SplineComponent = CreateDefaultSubobject<USplineComponent>("Spline");
+
+	if (SplineComponent)
+	{
+		SetRootComponent(SplineComponent);
+	}
+
+}
+
+void AWallSpline::OnConstruction(const FTransform& Transform)
+{
+	Super::OnConstruction(Transform);
+
+	if (!Mesh)
+	{
+		return;
+	}
+	for (int SplineCount = 0; SplineCount < (SplineComponent->GetNumberOfSplinePoints()); SplineCount++) {
+		USplineMeshComponent* SplineMesh = NewObject<USplineMeshComponent>(this, USplineMeshComponent::StaticClass());
+		SplineMesh->SetStaticMesh(Mesh);
+		SplineMesh->SetMobility(EComponentMobility::Movable);
+		SplineMesh->CreationMethod = EComponentCreationMethod::UserConstructionScript;
+		SplineMesh->RegisterComponentWithWorld(GetWorld());
+		SplineMesh->AttachToComponent(SplineComponent, FAttachmentTransformRules::KeepRelativeTransform);
+
+		
+
+		const FVector StartPos = SplineComponent->GetLocationAtSplinePoint(SplineCount, ESplineCoordinateSpace::Local);
+		const FVector StartTangent = SplineComponent->GetTangentAtSplinePoint(SplineCount, ESplineCoordinateSpace::Local);
+		const FVector EndPos = SplineComponent->GetLocationAtSplinePoint(SplineCount+1, ESplineCoordinateSpace::Local);
+		const FVector EndTangent = SplineComponent->GetTangentAtSplinePoint(SplineCount+1, ESplineCoordinateSpace::Local);
+		SplineMesh->SetStartAndEnd(StartPos, StartTangent, EndPos, EndTangent , true);
+		SplineMesh->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+
+	}
 }
 
 // Called when the game starts or when spawned
--- Source/UnrealAssigmentWeek3/Public/WallSpline.h
+++ Source/UnrealAssigmentWeek3/Public/WallSpline.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include <Components/SplineComponent.h>
 #include "WallSpline.generated.h"
 
 UCLASS()
@@ -15,6 +16,8 @@ public:
 	// Sets default values for this actor's properties
 	AWallSpline();
 
+	void OnConstruction(const FTransform& Transform) override;
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
@@ -23,4 +26,9 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Components")
+	USplineComponent* SplineComponent;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Components")
+	UStaticMesh* Mesh;
 };
